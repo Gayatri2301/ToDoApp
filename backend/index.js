7caf2b9b9277b498a14c8e1d6218173165f76715
Gayatri2301 <gayathriedibilli@gmail.com>
2024-08-07 00:26:33 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(cors({origin:'http://localhost:3000'}));
+app.use(cors());
 app.use(bodyParser.urlencoded({
     extended : true
 }));
